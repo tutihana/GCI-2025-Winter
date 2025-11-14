84d122203d1d34ed9d925154ed97b06cb3f596cb
piyopiyo <piyopiyobabyboy@gmail.com>
2025-11-14 16:13:32 +0900

    Perished rate by Pclass

---
+++ plotdata/Pclass.py
@@ -0,0 +1,23 @@
+import japanize_matplotlib                           # グラフに日本語を表示
+import numpy as np                                   # 数値計算や配列操作
+import pandas as pd                                  # 表形式のデータを扱う
+import matplotlib.pyplot as plt                      # 基本的なグラフ描画
+import seaborn as sns                                # きれいで便利な統計グラフ
+import missingno as msno                             # 欠損値の可視化
+
+from sklearn.preprocessing import LabelEncoder       # カテゴリ変数を数値に変換
+from sklearn.ensemble import RandomForestClassifier  # ランダムフォレスト分類器
+from sklearn.model_selection import train_test_split # データの分割
+from sklearn.metrics import accuracy_score           # 正解率の計算
+
+PATH = "C:\\Users\\piyop\\OneDrive\\デスクトップ\\GCI 2025 Winter\\data\\"
+train = pd.read_csv(PATH + 'train.csv') 
+test = pd.read_csv(PATH + 'test.csv')
+
+perished_rate = train.groupby('Pclass')['Perished'].mean()
+print(perished_rate)
+
+train.groupby('Pclass')['Perished'].mean().plot(kind='bar')
+plt.ylabel('Perished Rate')
+plt.title('Perished Rate by Pclass')
+plt.show()
